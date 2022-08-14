b08c6d35b39783457309425f568c60ad215b0fdf
Leonid <67646562+2ndAfterGod@users.noreply.github.com>
2022-08-14 17:34:53 +0300

    Add files via upload

--- Задание1/test.py
+++ Задание1/test.py
@@ -1,27 +1,59 @@
 import rasterio
 from flask import Flask, request
+from geomet import wkt
 
-point = [[160, 55.],] 
+cords = [[160, 55],] 
 elevation = 'srtm_N55E160.tif'
 
 app = Flask (__name__)
 
 @app.route('/elevation')
 def query_example():
-    
-    x = float(request.args.get('x'))
-    y = float(request.args.get('y'))
-    point[0][0],point[0][1]= x,y
+    wkt_req = request.args.get('WKT')
+    print(wkt_req)
+    ls = wkt.loads(wkt_req)
+    print(ls)
+    cords = []
+    if ls['type'] == 'LineString':
+        for i in range (len(ls['coordinates'])):
+            if len(ls['coordinates'][i]) == 2:
+                point = ls['coordinates'][i]
+                if 160 <= point[0] <= 161 and 55 <= point[1] <= 56:    
+                    buff = [[0,0],]
+                    buff[0][0],buff[0][1] = point[0],point[1]
+                    with rasterio.open(elevation) as src:
+                        heigh, = src.sample(buff)
+                    point.append(heigh[0])
+                
+                    cords.append(point)
+                else:
+                     return '<h1>Введенные координаты выходят за пределы рабочей области программы <h1>'
+            else:
+                return '<h1>Данные введены с ошибкой <h1>'    
+        return '<h1>Полученные координаты точек: {}<h1>'.format(cords)
+            
+            
 
-    with rasterio.open(elevation) as src:
-        z, = src.sample(point)
-
-    return '''<h1> Полученая точка ({}, {}, {})</h1>'''.format(x,y,z[0])
-
+    elif ls['type'] == 'Point':
+        if len(ls['coordinates']) == 2:    
+            point = ls['coordinates']
+            if 160 <= point[0] <= 161 and 55 <= point[1] <= 56:
+                buff = [[0,0],]
+                buff[0][0],buff[0][1] = point[0],point[1]
+                with rasterio.open(elevation) as src:
+                    heigh, = src.sample(buff)
+                point.append(heigh[0])
+            else:
+                     return '<h1>Введенные координаты выходят за пределы рабочей области программы <h1>'
+            return '<h1>Координаты полученной точки: {} <h1>'.format(point)
+        else:
+            return '<h1>Точка указана неверно  <h1>'  
+   
 @app.route('/')
 
 def hello():
-    return '<h1>Введите в адрес координаты точки<h1>'  
+
+    return '<h1>Введите в адрес координаты точки в формате POINT(x y) или несколько точек в формате LINESTRING(x y, x y, x y...)  <h1>'  
 
 if __name__ == '__main__':
     app.run(debug=True)
